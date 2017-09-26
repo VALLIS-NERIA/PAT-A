eacc709a612a6b05fd874707839b2aa032b45be0
VALLIS-NERIA <genius-littley@foxmail.com>
2017-09-26 10:59:58 +0800

    添加项目文件。

---
+++ 1001. A+B Format/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1001.A_B_Format {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
